b33c62185aa1e4e61d7c535ebdac316929adfa57
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2024-11-14 16:18:55 +0200

    Initial commit

---
+++ pkg/render/render.go
@@ -0,0 +1,94 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/TimofteRazvan/castle-event-booker/pkg/config"
+	"github.com/TimofteRazvan/castle-event-booker/pkg/models"
+)
+
+const pageTmplPath = "./templates/*.page.tmpl"
+const layoutTmplPath = "./templates/*.layout.tmpl"
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplate sets the config for the template package
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders templates using html tmpl
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var templateCache map[string]*template.Template
+	if app.UseCache {
+		templateCache = app.TemplateCache
+	} else {
+		templateCache, _ = CreateTemplateCache()
+	}
+
+	t, ok := templateCache[tmpl]
+	if !ok {
+		log.Fatal("Couldn't get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+
+	err := t.Execute(buf, td)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(pageTmplPath)
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob(layoutTmplPath)
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(layoutTmplPath)
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
