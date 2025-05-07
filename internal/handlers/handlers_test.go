5f2ae4e4125645585af053559b30de9df8e7e37c
Timofte Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2025-05-07 20:09:22 +0300

    - Added POST tests for handler. Fixed tiny horizontal overflow issue on frontend.

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -25,8 +26,22 @@ var tests = []struct {
 	{"banquet", "/banquet", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"booking", "/booking", "GET", []postData{}, http.StatusOK},
-	{"makeReservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
-	{"reservationSummary", "/reservation-summary", "GET", []postData{}, http.StatusOK},
+	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"reservation-summary", "/reservation-summary", "GET", []postData{}, http.StatusOK},
+	{"post-booking", "/booking", "POST", []postData{
+		{key: "start", value: "2022-05-26"},
+		{key: "end", value: "2022-05-27"},
+	}, http.StatusOK},
+	{"post-booking-json", "/booking-json", "POST", []postData{
+		{key: "start", value: "2022-05-26"},
+		{key: "end", value: "2022-05-27"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Razvan"},
+		{key: "last_name", value: "Smith"},
+		{key: "email", value: "razvan@gmail.com"},
+		{key: "phone", value: "0777777777"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -47,7 +62,20 @@ func TestHandlers(t *testing.T) {
 				t.Errorf("for %s, expected %d but got %d", test.name, test.expectedStatusCode, response.StatusCode)
 			}
 		} else {
+			values := url.Values{}
+			for _, param := range test.params {
+				values.Add(param.key, param.value)
+			}
 
+			response, err := testServer.Client().PostForm(testServer.URL+test.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if response.StatusCode != test.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", test.name, test.expectedStatusCode, response.StatusCode)
+			}
 		}
 	}
 }
