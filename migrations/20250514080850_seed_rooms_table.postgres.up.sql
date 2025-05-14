f7520b9b4a2c33846ff21868197a8472ddf8a151
Timofte Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2025-05-14 11:11:15 +0300

    feat: migrations for seeding rooms and restrictions tables

---
+++ migrations/20250514080850_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Knights'' Hall','2025-05-11 00:00:00.000','2025-05-11 00:00:00.000'),
+	 ('Banquet Hall','2025-05-12 00:00:00.000','2025-05-12 00:00:00.000'),
+	 ('Throne Room','2025-05-12 00:00:00.000','2025-05-12 00:00:00.000');
